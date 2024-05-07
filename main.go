d1437d45ae87024120ee8fe65d6b628d751fee9f
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2024-05-07 21:21:43 +0300

    fix: main

--- main.go
+++ main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"time"
 )
 
+const dumpPath = "enwiki-latest-abstract1.xml.gz"
+
 func main() {
-	var dumpPath, query string
-
-	flag.StringVar(&dumpPath, "p", "enwiki-latest-abstract1.xml.gz", "wiki abstract data dump")
-	flag.StringVar(&query, "q", "wild cat", "search query")
-	flag.Parse()
 	log.Println("Full text search in progress...")
-
 	start := time.Now()
 	docs, _ := LoadDocuments(dumpPath)
 	log.Printf("Loaded %d documents in %v", len(docs), time.Since(start))
@@ -23,7 +18,11 @@ func main() {
 	idx.Add(docs)
 	log.Printf("Indexed %d documents in %v", len(docs), time.Since(start))
 
-	start = time.Now()
+	SearchEngine("wild cat", idx, docs)
+}
+
+func SearchEngine(query string, idx Index, docs []Document) {
+	start := time.Now()
 	matchedIDs := idx.Search(query)
 	log.Printf("Search found %d documents in %v", len(matchedIDs), time.Since(start))
 	for _, id := range matchedIDs {
