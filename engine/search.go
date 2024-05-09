27a8d237aa189f5644d53dd44788dbe8a8815450
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2024-05-10 00:19:30 +0300

    feat: add UI for the search engine

--- engine/search.go
+++ engine/search.go
@@ -5,12 +5,30 @@ import (
 	"time"
 )
 
-func SearchEngine(query string, idx Index, docs []Document) {
+func Initialize() ([]Document, Index) {
+	const dumpPath = "enwiki-latest-abstract1.xml.gz"
+	log.Println("Full text search in progress...")
+	start := time.Now()
+	docs, _ := LoadDocuments(dumpPath)
+	log.Printf("Loaded %d documents in %v", len(docs), time.Since(start))
+
+	start = time.Now()
+	idx := make(Index)
+	idx.Add(docs)
+	log.Printf("Indexed %d documents in %v", len(docs), time.Since(start).Milliseconds())
+
+	return docs, idx
+}
+
+func Search(query string, idx Index, docs []Document) []Document {
+	var result []Document
 	start := time.Now()
 	matchedIDs := idx.Search(query)
 	log.Printf("Search found %d documents in %v", len(matchedIDs), time.Since(start))
 	for _, id := range matchedIDs {
 		doc := docs[id]
-		log.Printf("docID: %d\n docText:%s\n", id, doc.Text)
+		// log.Printf("docID: %d\n docText:%s\n", id, doc.Text)
+		result = append(result, doc)
 	}
+	return result
 }
--- main.go
+++ main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
+	"html/template"
 	"log"
-	"time"
+	"net/http"
 
 	"github.com/MhmoudGit/text-search-engine/engine"
 )
 
-const dumpPath = "enwiki-latest-abstract1.xml.gz"
+type Data struct {
+	Search  string
+	Results []engine.Document
+}
 
 func main() {
-	log.Println("Full text search in progress...")
-	start := time.Now()
-	docs, _ := engine.LoadDocuments(dumpPath)
-	log.Printf("Loaded %d documents in %v", len(docs), time.Since(start))
+	docs, idx := engine.Initialize()
 
-	start = time.Now()
-	idx := make(engine.Index)
-	idx.Add(docs)
-	log.Printf("Indexed %d documents in %v", len(docs), time.Since(start))
+	r := http.NewServeMux()
 
-	engine.SearchEngine("wild cat", idx, docs)
+	// Create a file server to serve static files
+	fs := http.FileServer(http.Dir("static"))
+	// Serve static files from the /static/ route
+	r.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Handle requests to the root path by serving the index.html file
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
+
+	// Parse the HTML template
+	tmpl := template.Must(template.ParseFiles("static/templates/index.html"))
+
+	// Define a handler function
+	r.HandleFunc("POST /templ", func(w http.ResponseWriter, r *http.Request) {
+		search := r.FormValue("search")
+		searchResults := engine.Search(search, idx, docs)
+
+		var data = Data{
+			Search:  search,
+			Results: searchResults,
+		}
+
+		// Execute the template, passing the data
+		err := tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	log.Println("Listening on port 8000...")
+	http.ListenAndServe("localhost:8000", r)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world"))
 }
